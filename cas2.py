f5985667670560e2bee47e20b1ccb57d05281f4f
godbygod379 <godbygod379@gmail.com>
2026-04-23 17:08:31 +0200

    dfgt

--- cas2.py
+++ cas2.py
@@ -1,5 +1,5 @@
 s = int(input("Zadaj pocet sekund: "))
-h = s // 3600
-m = (s % 3600) // 60
-sec = s % 60
-print(f"{s} sekund je {h} hodin, {m} minut a {sec} sekund.")
+hour = s // 3600
+minute = (s % 3600) // 60
+second = s % 60 
+print(f"{hour}:{minute}:{second}")
