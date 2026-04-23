29484e3c4138dedef9efcd45394de4e0f5540ab3
godbygod379 <godbygod379@gmail.com>
2026-04-23 16:49:44 +0200

    dfdwdf

--- kupenie.py
+++ kupenie.py
@@ -3,4 +3,4 @@ prijem = int(input("zadaj prijem: "))
 mam = int(input("zadaj kolko máš: "))
 mesiace = (ciel - mam) / prijem
 print(f"Potrebuješ {ciel - mam} aby si dosiahol svoj ciel")
-print(f"ak budeš mať {prijem} každý mesiac, tak ti bude trvať {mesiace} mesiacov aby si dosiahol svoj ciel")
+print(f"{mesiace} mesiacov aby si dosiahol svoj ciel")
