6baa85f6c33b8c9b2ad296e288b3abedbbb04bf9
godbygod379 <godbygod379@gmail.com>
2026-05-07 17:09:25 +0200

    hotovo

---
+++ month.py
@@ -0,0 +1,10 @@
+pack = int(input("Enter the number of packages in a day: "))
+drivers = int(input("Enter the number of drivers available: "))
+money = int(input("Enter the amount of monthly salary for each driver: "))
+trzby = pack * 30 * 2.50
+naklady = drivers * money
+
+if trzby > naklady:
+    print(f"you made a profit of {trzby - naklady}")
+elif naklady > trzby:
+    print(f"you made a loss of {naklady - trzby}")
