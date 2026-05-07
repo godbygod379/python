b9d2969dc3d15f9aeda8699f2d7d662b244d4789
godbygod379 <godbygod379@gmail.com>
2026-05-07 17:35:52 +0200

    hotovo

--- cena.py
+++ cena.py
@@ -3,21 +3,21 @@ cm = float(input("how long is it? "))
 price = float(input("how much does it cost? "))
 final_price = 0
 if kg <= 1:
-    price = price + 3.20
+    final_price = final_price + 3.20
 elif kg <= 5:
-    price = price + 4.80
+    final_price = final_price + 4.80
 elif kg <= 20:
-    price = price + 7.90
+    final_price = final_price + 7.90
 else:
-    price = price + 12.50
+    final_price = final_price + 12.50
 
 if cm > 80:
-    price = price + 3.00
+    final_price = final_price + 3.00
 if price > 1000:
-    price = price +0.03 * price
+    final_price = final_price + 0.03 * price
 elif price > 500:
-    price = price + 0.02 * price
+    final_price = final_price + 0.02 * price
 elif price > 100:
-    price = price + 0.01 * price
+    final_price = final_price + 0.01 * price
 
-print(f"The final price is: {price}")
+print(f"The final price is: {final_price} euros")
