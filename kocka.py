08c494b004cfe208ae9ac0f6f2185aad669f0c1d
godbygod379 <godbygod379@gmail.com>
2026-05-14 16:24:37 +0200

    ok

--- kocka.py
+++ kocka.py
@@ -1,9 +1,8 @@
 import random 
 
-roll = random.randint(1, 6)
-print("Hodil si:", roll)
-if roll == 6:
-    print("vyhral si")
-else:
-    print("nevies hrat")
-    
+roll = 0
+
+while roll != 6:
+    roll = random.randint(1, 6)
+    print(roll)
+print("Koniec")
