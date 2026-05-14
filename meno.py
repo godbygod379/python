b94b2977d7d4af410c27cff221c3ce4683a7424e
godbygod379 <godbygod379@gmail.com>
2026-05-14 16:37:33 +0200

    ok

--- meno.py
+++ meno.py
@@ -1,7 +1,7 @@
 meno = input("What is your name? ")
 print("Hello")
 cislo = 0
-while cislo < 10:
+while cislo < 1000
     print(f"Hello {meno}")
     cislo = cislo + 1
-
+print(f"you have said hello {cislo} times")
