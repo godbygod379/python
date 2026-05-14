4af20a68d344b24453656e9b8edb1494a84533c9
godbygod379 <godbygod379@gmail.com>
2026-05-14 16:45:28 +0200

    done finale

--- kasino.py
+++ kasino.py
@@ -5,15 +5,16 @@ go = 1
 result = 0
 choose = 1
 while  go == 1:
+    print(f"You have {money} dollars.")
     bet = int(input("How much do you want to bet? "))
     if bet > money:
         print("You don't have enough money to place that bet.")
     else:
-    choose = input("Choose a number between 0 and 10: ")
-    print("Bet placed.")
-    money = money - bet
-    print("Spinning the wheel...")
-    result = random.randint(0, 10)
+        choose = input("Choose a number between 0 and 10: ")
+        print("Bet placed.")
+        money = money - bet
+        print("Spinning the wheel...")
+        result = random.randint(0, 10)
     if result == choose:
         print("Congratulations! You won!")
         money = money + bet * 2
