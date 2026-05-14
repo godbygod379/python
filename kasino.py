3b48213043cb6231ae291b7b8a285735b29dcfe7
godbygod379 <godbygod379@gmail.com>
2026-05-14 16:50:36 +0200

    pls

--- kasino.py
+++ kasino.py
@@ -28,4 +28,4 @@ while  go == 1:
     go = int(input("Do you want to play again? (1 for yes, 0 for no) "))
 if money > 0:
      print(f"your balance is {money}")
-print(f"You have {money} left and you lost {lost} dollars.")
+print(f"You have {money} left and you lost {lost} dollars bye")
