b4cd092db761286e3cbe83bb4cdefcd9f82ea59c
godbygod379 <godbygod379@gmail.com>
2026-05-28 17:40:29 +0200

    dgfdg

--- tkinker.py
+++ tkinker.py
@@ -7,8 +7,7 @@ c.create_rectangle(350, 420, 370, 520, fill="black")
 c.create_rectangle(430, 420, 450, 520, fill="black")  
 c.create_rectangle(320, 220, 480, 420, fill="lightgray", outline="lightgray")
 c.create_oval(340, 100, 460, 220, fill="blue", outline="blue")
-c.create_rectangle(360, 140, 440, 180, fill="orange", outline="orange")
-c.create_rectangle(370, 148, 430, 172, fill="yellow", outline="yellow")
-c.create_polygon(400, 20, 370, 110, 430, 110, fill="purple", outline="purple")
+
+
 
 c.mainloop()
