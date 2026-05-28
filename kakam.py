921555c0dcf8ce70f6a9ef4d451eafb64cf9a0d1
godbygod379 <godbygod379@gmail.com>
2026-05-28 16:24:35 +0200

    dfngbhjnjf

--- kakam.py
+++ kakam.py
@@ -1,4 +1,10 @@
-oka = int(input("Enter a number: "))
-
-for i in range (1, oka + 1):
-    print("#" * i)
+x = int(input("Enter a number: "))
+x *= 2
+m = x
+h = 0
+for i in range (1, x, 2):
+    if h == 0:
+        print(" " * (m + 1) + "#" * i)
+        h += 1
+    print(" " * m + "#" +"-" * i + "#")
+    m -= 1
