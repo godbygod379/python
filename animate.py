c2a5b1cd68fedcb610c28a1802d9b8c1aad3aa19
godbygod379 <godbygod379@gmail.com>
2026-06-04 16:52:08 +0200

    asdsasd

--- animate.py
+++ animate.py
@@ -1,14 +1,18 @@
 import tkinter
 import random
 
-c = tkinter.Canvas(width=800, height=600)
+c = tkinter.Canvas(width=1920, height=1080)
 c.pack()
 for i in range(100):
-    x = random.randint(0, 800)
-    y = random.randint(0, 600)
+    x = random.randint(0, 1920)
+    y = random.randint(0, 1080)
     sirka = random.randint(20, 100)
     vyska = random.randint(20, 100)
-    c.create_rectangle(x, y, x + sirka, y + vyska, outline="black", fill="#ff0000")  
+    r = random.randit(0, 255)
+    g = random.randit(0, 255)
+    b = random.randit(0, 255)
+    color = f"#{r:02x}{g:02x}{b:02x}"
+    c.create_rectangle(x, y, x + sirka, y + vyska, outline=color, fill=color)  
 
     c.after(100)
     c.update()
