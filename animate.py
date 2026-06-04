e905ba8a991fe1e66163b98c98a75c1518a4e1a5
godbygod379 <godbygod379@gmail.com>
2026-06-04 16:53:39 +0200

    3eddf

--- animate.py
+++ animate.py
@@ -8,9 +8,9 @@ for i in range(100):
     y = random.randint(0, 1080)
     sirka = random.randint(20, 100)
     vyska = random.randint(20, 100)
-    r = random.randit(0, 255)
-    g = random.randit(0, 255)
-    b = random.randit(0, 255)
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
     color = f"#{r:02x}{g:02x}{b:02x}"
     c.create_rectangle(x, y, x + sirka, y + vyska, outline=color, fill=color)  
 
