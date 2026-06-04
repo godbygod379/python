7ba3e586c6887acd887af49f5a30a41ecbb97c0c
godbygod379 <godbygod379@gmail.com>
2026-06-04 17:15:23 +0200

    ggh

--- animate.py
+++ animate.py
@@ -1,3 +1,4 @@
+"""
 import tkinter
 import random
 
@@ -11,11 +12,40 @@ for i in range(1000000):
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
+    color2 = f"#{(255 - r):02x}{(255 - g):02x}{(255 - b):02x}"
     color = f"#{r:02x}{g:02x}{b:02x}"
-    c.create_rectangle(x, y, x + sirka, y + vyska, outline=color, fill=color)  
+    c.create_rectangle(x, y, x + sirka, y + vyska, outline=color2, fill=color, width = 5)  
 
     c.after(1)
     c.update()
+
+
+c.mainloop()
+"""
+
+
+import tkinter
+import random
+
+
+x = 0
+y = 0
+
+c = tkinter.Canvas(width=1920, height=1080)
+c.pack()
+
+for i in range(1000000):
+    c.delete("all")
+    c.create_rectangle(x, y, x + 50, y + 50, fill = "black", outline = "black")
+
+    x += 1
+    y += 1
+
+    c.after(1)
+    c.update()
+
+
+
 
 
 c.mainloop()
