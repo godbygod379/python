2e5f3ad81b16205e8d252e4665647c871e9e594c
godbygod379 <godbygod379@gmail.com>
2026-06-04 16:06:36 +0200

    dfs

--- animate.py
+++ animate.py
@@ -3,8 +3,8 @@ import random
 
 c = tkinter.Canvas(width=800, height=600)
 c.pack()
-
-c.create_rectangle(random.randint(0, 800), random.randint(0, 600), random.randint(0, 800), random.randint(0, 600), outline="black")  
+for i in range(100):
+    c.create_rectangle(random.randint(0, 800), random.randint(0, 600), random.randint(0, 800), random.randint(0, 600), outline="black")  
 
 
 c.mainloop()
