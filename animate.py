47b10fa14989ca76c9a978e585ab033e8b335667
godbygod379 <godbygod379@gmail.com>
2026-06-04 16:32:58 +0200

    trtr

--- animate.py
+++ animate.py
@@ -8,7 +8,7 @@ for i in range(100):
     y = random.randint(0, 600)
     sirka = random.randint(20, 100)
     vyska = random.randint(20, 100)
-    c.create_rectangle(x, y, x + sirka, y + vyska, outline="black")  
+    c.create_rectangle(x, y, x + sirka, y + vyska, outline="black", fill="#ff0000")  
 
     c.after(100)
     c.update()
