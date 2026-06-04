5e299eece70a1a48ad6c0fad11a163748d2872fb
godbygod379 <godbygod379@gmail.com>
2026-06-04 16:05:48 +0200

    sda

--- animate.py
+++ animate.py
@@ -4,7 +4,7 @@ import random
 c = tkinter.Canvas(width=800, height=600)
 c.pack()
 
-c.create_rectangle(random.randint(0, 800), random.randint(0, 600), random.randint(0, 800), random.randint(0, 600), fill="black")  
+c.create_rectangle(random.randint(0, 800), random.randint(0, 600), random.randint(0, 800), random.randint(0, 600), outline="black")  
 
 
 c.mainloop()
