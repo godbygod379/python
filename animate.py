57d8bf48b7bf1b30d26e56c2096b47be17b5dc7a
godbygod379 <godbygod379@gmail.com>
2026-06-04 16:28:36 +0200

    sad

--- animate.py
+++ animate.py
@@ -4,7 +4,14 @@ import random
 c = tkinter.Canvas(width=800, height=600)
 c.pack()
 for i in range(100):
-    c.create_rectangle(random.randint(0, 800), random.randint(0, 600), random.randint(0, 800), random.randint(0, 600), outline="black")  
+    x = random.randint(0, 800)
+    y = random.randint(0, 600)
+    sirka = random.randint(20, 100)
+    vyska = random.randint(20, 100)
+    c.create_rectangle(x, y, x + sirka, y + vyska, outline="black")  
+
+    c.after(100)
+    c.update()
 
 
 c.mainloop()
