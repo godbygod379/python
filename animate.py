0209128d5cbbbfdfb6c8a7b0a4aab701b2228afc
godbygod379 <godbygod379@gmail.com>
2026-06-04 16:03:45 +0200

    zc

---
+++ animate.py
@@ -0,0 +1,10 @@
+import tkinter
+import random
+
+c = tkinter.Canvas(width=800, height=600)
+c.pack()
+
+c.create_rectangle(random.randint(0, 800), random.randint(0, 600), random.randint(0, 800),random.randint(0, 600), fill="rand  om", outline="random")  
+
+
+c.mainloop()
